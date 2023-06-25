cc390337ab73a739b61a1f1dd93ff754d4c43e22
jonatas.peixoto.zup <jonatas.peixoto@zup.com.br>
2023-06-25 17:50:20 -0300

    Estilizando componentes no próprio arquivo

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet} from 'react-native';
 
 const Header = () => {
     return (
-        <View style={{backgroundColor:'rgb(84,50,168)', padding:20, width:'100%'}}>
-            <Text style={{textAlign:'center', fontSize:30, color:'white'}}>App Música</Text>
+        <View style={styles.view}>
+            <Text style={styles.textStyles}>App Música</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    view:{
+        backgroundColor:'rgb(84,50,168)',
+        padding:20,
+        width:'100%'
+    },
+    textStyles:{
+        textAlign:'center', 
+        fontSize:30, 
+        color:'white'
+    }
+});
 
 export default Header;
