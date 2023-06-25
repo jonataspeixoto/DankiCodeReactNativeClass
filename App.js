cc2db9d87ca80a510a1370b73e705696c90eb1af
jonatas.peixoto.zup <jonatas.peixoto@zup.com.br>
2023-06-25 16:48:44 -0300

    Criando componentes personalizados no mesmo arquivo

--- App.js
+++ App.js
@@ -3,16 +3,38 @@ import React from 'react'
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
 export default class App extends React.Component {
+  Header = () => {
+    return(
+      <Text>Home | Sobre | Contato</Text>
+    );
+  }
+  
+  Body = () => {
+    return(
+      <Text>Conteúdo do meu app.</Text>
+    );
+  }
+
+  Footer = () => {
+    return(
+      <Text>Rodapé do meu app.</Text>
+    );
+  }
+
   render(){
     return (
-      <View>
-        <Text>Minha imagem:</Text>
-        <Image style={{width:50, height:100}}
-          source={{
-            uri: 'http://reactnative.dev/img/tiny_logo.png'
-          }}
-        />
+      <View style={{marginTop:20}}>
+        <this.Header></this.Header>
+        <this.Body></this.Body>
+        <this.Footer></this.Footer>
+        <Botao></Botao>
       </View>
     );  
   }
+}
+
+const Botao = () => {
+  return(
+    <Button title='Clique'></Button>
+  );
 }
