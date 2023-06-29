c2cf38a049754d981dcf7eed34c6305a53a1056c
jonatas.peixoto.zup <jonatas.peixoto@zup.com.br>
2023-06-29 01:17:04 -0300

    Dica: Detectando se é iOS ou Android

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, ScrollView, Button, LogBox } from 'react-native';
+import { Text, View, ScrollView, Button, Platform } from 'react-native';
 import Header from './components/Header.js';
 import Body from './components/Body.js';
 
@@ -7,25 +7,21 @@ export default class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.mostrarHeader = true;
-    LogBox.ignoreAllLogs(true);
-    console.warn("Testes");
   }
 
   mostrarConteudo() {
-    var mostrarBody = 1;
-    if (this.mostrarHeader && mostrarBody == 1) {
-      return (
-        <View>
-          <Header></Header>
-          <Body></Body>
-        </View>
-      );
-    } else {
-      return (
-        <Body></Body>
-      );
+    var conteudo = "";
+    
+    if(Platform.OS == "android"){
+      conteudo = "Olá! Você está utilizando android!"
+    } else{
+      conteudo = "Olá, você é usuário iOS!"
     }
+    return (
+      <View>
+        <Text>{conteudo}</Text>
+      </View>
+    );
   }
 
   render() {
