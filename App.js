5ec1f5090ecd57a796c8184ed6a9e6f57c5e50ce
jonatas.peixoto.zup <jonatas.peixoto@zup.com.br>
2023-06-29 00:53:01 -0300

    Condições na renderização através de expressões

--- App.js
+++ App.js
@@ -1,15 +1,35 @@
 import React from 'react';
-import { Text, View, ScrollView } from 'react-native';
+import { Text, View, ScrollView, Button } from 'react-native';
 import Header from './components/Header.js';
 import Body from './components/Body.js';
 
-
 export default class App extends React.Component {
+  
+  constructor(props){
+    super(props);
+    this.mostrarTexto = true;
+    this.mostrarBotaoComTexto = true;
+  }
+
   render(){
+
     return (
       <ScrollView style={{marginTop:20, flex:1}}>
-        <Header></Header>
-        <Body></Body>
+        {
+          (this.mostrarTexto)
+          ?
+          <Text>Texto</Text>
+          :
+          <View></View>
+        }
+        {
+          (this.mostrarBotaoComTexto)
+          ?
+          <Button title="Botao com texto"></Button>
+          :
+          <Button title="Botao sem texto"></Button>
+        }
+
       </ScrollView>
     );  
   }
