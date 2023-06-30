6cb743933871e958fd130adb8a594b2d2e16b1bf
jonatas.peixoto.zup <jonatas.peixoto@zup.com.br>
2023-06-29 21:57:27 -0300

    Ciclo da aplicação

--- App.js
+++ App.js
@@ -1,40 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';
 import Body from './components/Body';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      contador: 0,
-      pagina: "home"
-    }
-
-    setInterval(() => {
-      this.setState({ pagina: "contato" });
-    }, 2000)
-  }
-
-  alterarAba = (pagina) =>{
-    this.setState({pagina: pagina})
-  }
-
-  render() {
-    if (this.state.pagina == "home") {
-      return (
-        <View style={{ marginTop: 30 }}>
-          <View><Text>Home</Text></View>
-          <Button onPress={()=>this.alterarAba('contato')} title = "Clique para ir ao contato"></Button>
-        </View>
-      );
-    } else if (this.state.pagina == "contato") {
-      return (
-        <View style={{ marginTop: 30 }}>
-          <View><Text>Contato</Text></View>
-          <Button onPress={()=>this.alterarAba('home')} title = "Clique para ir a home"></Button>
-        </View>);
-    }
-  }
+export default function App(){
+  useEffect(() => {
+    alert('meu hook de efeito tá legal!')
+  })
+  return(
+    <View style={{marginTop:20}}>
+      <Text>Funcionando</Text>
+    </View>
+  )
 }
