cf7b552ad0f7741bbc331d114a712370ee366b01
jonatas.peixoto.zup <jonatas.peixoto@zup.com.br>
2023-06-29 22:06:10 -0300

    Hooks

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';
 import Body from './components/Body';
 
-export default function App(){
-  useEffect(() => {
-    alert('meu hook de efeito tá legal!')
-  })
-  return(
-    <View style={{marginTop:20}}>
-      <Text>Funcionando</Text>
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  const [nome, setNome] = useState("Guilherme");
+
+  const [lista, setarLista] = useState(['Feijão', 'Arroz', 'Pão']);
+  
+  return (
+    <View style={{ marginTop: 20 }}>
+      <Button onPress={() => setarLista([...lista, 'Salada'])} title="Clique em mim!"></Button>
+      {
+        lista.map((val => {
+          return (<Text>{val}</Text>)
+        }))
+      }
     </View>
   )
 }
